e0fa1c4570950318e80fc468758bd616e6496998
KhallanJ <khallanjamal20@gmail.com>
2026-05-01 15:18:55 -0400

    Push before New Branch - WebPage

--- webscrapping.py
+++ webscrapping.py
@@ -3,8 +3,14 @@ from bs4 import BeautifulSoup
 import re
 
 class WebScrapping:
-    def extract_text_from_URL(self, url):     
-        response = requests.get(url)
+    def extract_text_from_URL(self, url):  
+        # Deter Wiki from thinking this program is a bot
+        headers = {
+            "User-Agent": "Mozilla/5.0"
+        }
+
+        response = requests.get(url, headers=headers)
+        text = ""
 
         # Check if the request was successful
         if response.status_code == 200:
@@ -47,7 +53,7 @@ class WebScrapping:
     
     
     
-    def interactionMenu(self, text):
+    def interactionMenu(self, text, word_counts):
         def count_word():
             word = input("What word are you looking for")
             print(f"\nThe word \"{word}\" appears:" , word_counts.get(word, 0) , "times")
@@ -65,39 +71,46 @@ class WebScrapping:
             print("\n")
             query_words = query.lower().split()
             
-            # Splits the text into chunks, split by periods
+            # Convert all ending punctuaiton to periods, then splits on them
             clean_punctuation = text.replace("!", ".").replace("?", ".").replace(",", " ")
             sentences_words = clean_punctuation.split(".")
                         
             results = []
+            # Filler words are exempt from Search
             ignore_words = ["a", "the", "with", "of", "i", "this", "that", "to", "and", "an", "is", "what"]
-            query_words = [w for w in query_words if w not in ignore_words]
-                 
-            print(type(sentences_words))
-            # print(sentences_words[:5])   
+            query_words = [w for w in query_words if w not in ignore_words] # Ignore the word, if it is found in ingore_words
             
+            total_possible_weight = sum(1 / word_counts[w] for w in query_words if w in word_counts)
+
             for sentence in sentences_words:
+                # Normalize Punctuation
                 sentences_words = re.sub(r'[^\w\s]', '', text)
                 matches = 0
+                # Find the amount of matches between query and sentance
                 for word in query_words:
-                    if word in sentence.split():
-                        matches += 1 
-                        
+                    if word in sentence.split() and word in word_counts:
+                        matches += 1 / word_counts[word]
+                  
+                # Determine score for revelance      
                 score = 0
-                if len(query_words) > 0:          
-                    score = matches / len(query_words)
+                if total_possible_weight > 0:          
+                    score = matches / total_possible_weight
                 else:
                     print("Cannot divide by Zero")
        
+                # Append scores to the results list
                 if score > 0:
                     results.append((sentence, score))
                 
             results.sort(key=lambda x: x[1], reverse=True)
+            print(sentence)
+            print(matches) 
             
-            
+            # If results[] is empty (no words have been found) print error 
             if not results:
                 print("Error 404: Does not exist\n")
                     
+            # Print formatting
             for result in results[:3]:
                 print("\"", result[0], "\"", "\tScore:", result[1], "\n")
             
@@ -124,12 +137,12 @@ class WebScrapping:
 if __name__ == "__main__":
     WS = WebScrapping()
     
-    text = WS.extract_text_from_URL("https://www.britannica.com/topic/LEGO")
+    text = WS.extract_text_from_URL("https://en.wikipedia.org/wiki/Lego")
     text = WS.to_Lower(text)
     words = WS.split(text)
     
     word_counts = WS.counting_words(words)
-    WS.interactionMenu(text)
+    WS.interactionMenu(text, word_counts)
     
     #https://www.britannica.com/topic/LEGO
     #https://httpbin.org/html
